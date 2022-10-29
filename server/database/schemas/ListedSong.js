c78436cc4468ffc03eb29ff2357c464f90e4d6e0
DAVID NAGEB <davidnageb@DAVIDs-MacBook-Pro.local>
2022-10-29 14:57:37 -0400

    Created ListedSong Schema

---
+++ server/database/schemas/ListedSong.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose")
+const listedSongSchema =new mongoose.Schema({
+  
+creator:{
+  type:mongoose.SchemaTypes.ObjectId,
+  required: true  
+},
+song:{
+type:mongoose.SchemaTypes.ObjectId,
+required: true  
+},
+formats:{
+type:[mongoose.SchemaTypes.ObjectId],
+required: true
+},
+Price: {
+    type: Number,
+    required: true
+  }
+
+})
+
+module.exports=mongoose.model("listedSong",listedSongSchema)
+
+
+
+
