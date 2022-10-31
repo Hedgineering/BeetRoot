9ddefc93d0bad01b50499aef5ab4f64d33ce5cd4
Daniel Chen <chendaniel03@gmail.com>
2022-10-31 14:06:25 -0400

    Fix formatting issues

--- server/database/schemas/History.js
+++ server/database/schemas/History.js
@@ -1,24 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const historySchema = new mongoose.Schema({
-    user_id:{
-        type: mongoose.SchemaTypes.ObjectID,
-        required:true
-    },
-    streams:{
-        type: [{
-            song:{
-                type:mongoose.SchemaTypes.ObjectID,
-                required:true
-            },
-            times_streamed:{
-                type: Number,
-                default:0,
-                required: true
-            }
-        }],
-        required:true
-    }
+  user_id: {
+    type: mongoose.SchemaTypes.ObjectID,
+    required: true,
+  },
+  streams: {
+    type: [
+      {
+        song: {
+          type: mongoose.SchemaTypes.ObjectID,
+          required: true,
+        },
+        times_streamed: {
+          type: Number,
+          default: 0,
+          required: true,
+        },
+      },
+    ],
+    required: true,
+  },
 });
 
-module.exports = mongoose.model('History', historySchema)
+module.exports = mongoose.model("History", historySchema);
