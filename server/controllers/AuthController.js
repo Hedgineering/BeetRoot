f84b831e0e623681d42145ba686762a1224095da
Rahul Hegde <rhegde.hrahul@gmail.com>
2022-11-05 17:36:36 -0400

    prevented duplicate usernames users and fixed query errors for user registration

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -23,56 +23,88 @@ const {roleModel} = require("../database/schemas/Role");
 // Endpoints for Registration and Login
 // ====================================
 const register = (req, res) => {
-  //Increase the amount in gensalt to increase security but will slow down registration significantly.
-  const salt = bcrypt.genSalt(10);
-  const hashed_password = bcrypt.hash(req.body.password, salt);
+  if(!req.body || !req.body.password) {
+    res.status(400).json({message: "Password is required"});
+    return;
+  }
+  let requestedRoleStrings = [];
+  if(!req.body.roles || req.body.roles.length === 0) {
+    requestedRoleStrings.push("Listener");
+  } else {
+    requestedRoleStrings = Array.from(req.body.roles);
+  }
+  if(!req.body.username || !req.body.email || !req.body.firstName || !req.body.lastName || !req.body.status) {
+    res.status(400).json({message: "All fields are required"});
+    return;
+  }
 
-  // requested roles to be used for applying roles if valid
-  let requestedRoles = [];
-
-  // validate request roles
-  const roles = roleModel.find({}).select({name: 1, _id: 0}).then((roles) => {
-    const userRolesSet = new Set(req.body.roles);
-
-    // check if the roles in the request body are only the ones in the database
-    if (userRolesSet.size !== req.body.roles.length) {
-      res.status(400).send({ message: "Roles must be valid. You have too many types of roles!" });
+  userModel.findOne({username: req.body.username}, (err, user) => {
+    if(err) {
+      console.log(err);
+    }
+    if(user) {
+      res.status(400).json({message: "Username already exists"});
       return;
     }
-    roles.every((role) => {
-      // We don't allow external users to create admin users
-      if (!userRolesSet.has(role.name) && role.name !== "Admin") {
-        res.status(400).send({ message: "Roles must be valid. Incorrect roles requested." });
-        return;
-      }
+    // Make sure that req.body.roles doesn't contain "Admin" role
+    if(requestedRoleStrings.includes("Admin")) {
+      res.status(400).json({message: "Admin role cannot be assigned to a user"});
+      return;
+    }
+  
+    //Increase the amount in gensalt to increase security but will slow down registration significantly.
+    const saltRounds = 10;
+    bcrypt.hash(req.body.password, saltRounds, function(err, hashedPassword) {
+      // Store hash in database here
+      // requested roles to be used for applying roles if valid
+      let requestedRoles = [];
+    
+      // validate request roles
+      const roles = roleModel.find({}).select({name: 1, _id: 1}).then(async (roles) => {
+        const userRolesSet = new Set(roles.map((role) => role.name));
+        console.log(userRolesSet);
+    
+        // check if the roles in the request body are only the ones in the database
+        if (userRolesSet.size < requestedRoleStrings.length) {
+          res.status(400).json({ message: "Roles must be valid. You have too many types of roles!" });
+          return;
+        }
+        for(let role of requestedRoleStrings) {
+          console.log('checking role: ' + role);
+          if (!userRolesSet.has(role)) {
+            res.status(400).json({ message: "Roles must be valid. Incorrect roles requested." });
+            return;
+          }
+        }
+  
+        requestedRoleStrings = new Set(requestedRoleStrings);
+        console.log(roles);
+    
+        // if all roles are valid, then add their roleIds to the requestedRoles array
+        requestedRoles = roles.filter((role) => requestedRoleStrings.has(role.name)).map((role) => role._id);
+        console.log("requestedRoles: ", requestedRoles);
+        try {
+          await userModel.create([
+            {
+              username: req.body.username,
+              firstName: req.body.firstName,
+              lastName: req.body.lastName,
+              password: hashedPassword,
+              email: req.body.email,
+              status: req.body.status || "Normal",
+              roles: requestedRoles,
+            }
+          ]);
+          res.status(200).json({ message: "User registered successfully!" });
+        }
+        catch(error) {
+          // only log error internally, exposing error to user is a security risk
+          console.log(error);
+          res.status(500).json({ message: "Error registering user" });
+        };
+      });
     });
-
-    // if all roles are valid, then add their roleIds to the requestedRoles array
-    requestedRoles = roles.filter((role) => userRolesSet.has(role.name)).map((role) => role._id);
   });
-
-  userModel
-    .find({})
-    .then(async (users) => {
-      if (users.length === 0) {
-        await userModel.create([
-          {
-            username: req.body.username,
-            firstName: req.body.first_name,
-            lastName: req.body.last_name,
-            password: hashed_password,
-            email: req.body.email,
-            status: req.body.status || "Normal",
-            roles: requestedRoles,
-          },
-        ]);
-      }
-    })
-    .catch((error) => {
-      // only log error internally, exposing error to user is a security risk
-      console.log(error);
-      res.status(500).send({ message: "Error registering user" });
-    });
 };
 
 const login = (req, res) => {
