3ced2f093bd9e29b12299690e8e1b7e7a97fe13a
Daniel Chen <chendaniel03@gmail.com>
2022-11-06 00:17:28 -0400

    Adjust schema temporarily for demo

--- server/database/schemas/Comment.js
+++ server/database/schemas/Comment.js
@@ -1,32 +1,47 @@
 const mongoose = require("mongoose");
-const {userModel} = require("./User");
+const { userModel } = require("./User");
+const { listedSongModel } = require("./ListedSong");
 
 const commentSchema = new mongoose.Schema({
-  posted_by: {
+  listedSong: {
+    //type: mongoose.SchemaTypes.ObjectId,
+    type: String,
+    required: true,
+    ref: listedSongModel,
+  },
+  postedBy: {
     type: mongoose.SchemaTypes.ObjectId,
     required: true,
     ref: userModel,
   },
-  replied_by: {
-    type: [{type: mongoose.SchemaTypes.ObjectId, ref: "Comment"}],
+  repliedBy: {
+    type: [{ type: mongoose.SchemaTypes.ObjectId, ref: "Comment" }],
     required: true,
+    default:[]
   },
   message: {
     type: String,
     required: true,
   },
+  //True if string is not empty. String will contain reason comment was flagged.
   flagged: {
-    type: Boolean,
+    type: String,
     required: true,
+    default: "",
   },
   replies: {
     type: Number,
     required: true,
+    default: 0,
   },
   votes: {
     type: Number,
     required: true,
+    default: 0,
   },
 });
 
-module.exports = mongoose.model("Comment", commentSchema);
+module.exports = {
+  commentModel: mongoose.model("Comment", commentSchema),
+  commentSchema,
+};
