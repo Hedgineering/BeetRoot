376aab2bea0fdd5ff88886e04c4c3681cef2d3ee
Rahul Hegde <rhegde.hrahul@gmail.com>
2022-11-06 18:09:25 -0500

    added listing validation

--- server/controllers/CommentController.js
+++ server/controllers/CommentController.js
@@ -15,47 +15,63 @@ if (!env) {
 // General Require Statements
 // ==========================
 const jwt = require("jsonwebtoken");
+const {SchemaType} = require("mongoose");
 const { userModel } = require("../database/schemas/User");
 const { commentModel } = require("../database/schemas/Comment");
+const { listedSongModel } = require("../database/schemas/ListedSong");
 
 // ==============================
 // Endpoints for Posting Comments
 // ==============================
-const postComment = (req, res, next) => {
+const postComment = async (req, res, next) => {
   const token = req.body.token;
   const secret =
     process.env.ACCESS_TOKEN_SECRET ||
     env["ACCESS_TOKEN_SECRET"] ||
     "HedgineeringIsAwesome";
 
-  jwt.verify(token, secret, (err, verifiedJwt) => {
+  let listingId;
+  try {
+    listingId = new SchemaType.ObjectId(req.body.listingId);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ error: "Invalid listing ID" });
+  }
+
+  await listedSongModel.find({_id: listingId}, (err, listing) => {
     if (err) {
-      res.status(400).json({ message: "Invalid Token" });
-      return;
-    } else {
-      userModel.findOne({ username: verifiedJwt.username }, (err, user) => {
-        if (err) {
-          console.log(err);
-          next(err);
-        }
-        if (user) {
-          commentModel.create([
-            {
-              listedSong: req.params.listingId,
-              postedBy: user._id,
-              message: req.body.comment,
-            },
-          ]);
-          return;
-        }
-      });
-      console.log(verifiedJwt.username);
-      console.log(req.params.listingID);
-      console.log(req.body.comment);
-      res.status(200).json({ message: "Comment Posted!" });
-      //verifiedJwt is object of verified data
+      console.log(err);
+      return res.status(400).json({ error: "Invalid listing ID" });
     }
-  });
+    if (listing.length === 0) {
+      return res.status(400).json({ error: "Invalid listing ID" });
+    }
+
+    jwt.verify(token, secret, (err, verifiedJwt) => {
+      if (err) {
+        res.status(400).json({ message: "Invalid Token" });
+        return;
+      } else {
+        userModel.findOne({ username: verifiedJwt.username }, (err, user) => {
+          if (err) {
+            console.log(err);
+            next(err);
+          }
+          if (user) {
+            commentModel.create([
+              {
+                listedSong: req.params.listingId,
+                postedBy: user._id,
+                message: req.body.comment,
+              },
+            ]);
+            return;
+          }
+        });
+        res.status(200).json({ message: "Comment Posted!" });
+      }
+    });
+  })
 };
 
 module.exports = {
