35bdda3e774bd0716f2f531ddf64ef115b99e410
project-flytrap <77651264+project-flytrap@users.noreply.github.com>
2022-11-05 23:02:32 -0400

    Added Multer Dependency

---
+++ server/controllers/AudioController.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// =========================
+// Get Environment Variables
+// =========================
+const path = require("path");
+const rootDir = path.resolve(__dirname, ".");
+const env = require("dotenv").config({ path: `${rootDir}/.env` }).parsed;
+
+if (!env) {
+  console.log("Environment variables file not found");
+}
+
+// ==========================
+// General Require Statements
+// ==========================
