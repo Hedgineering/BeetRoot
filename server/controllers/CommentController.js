078fe5bbe3b19c5f78a839d3a46c24d9c282016f
Rahul Hegde <rhegde.hrahul@gmail.com>
2022-11-06 18:25:08 -0500

    fixed validation bug on comment posting

--- server/controllers/CommentController.js
+++ server/controllers/CommentController.js
@@ -15,7 +15,7 @@ if (!env) {
 // General Require Statements
 // ==========================
 const jwt = require("jsonwebtoken");
-const {SchemaType} = require("mongoose");
+const { Types } = require("mongoose");
 const { userModel } = require("../database/schemas/User");
 const { commentModel } = require("../database/schemas/Comment");
 const { listedSongModel } = require("../database/schemas/ListedSong");
@@ -32,13 +32,15 @@ const postComment = async (req, res, next) => {
 
   let listingId;
   try {
-    listingId = new SchemaType.ObjectId(req.body.listingId);
+    listingId = new Types.ObjectId(req.params.listingId);
   } catch (error) {
     console.log(error);
     return res.status(400).json({ error: "Invalid listing ID" });
   }
 
-  await listedSongModel.find({_id: listingId}, (err, listing) => {
+  console.log(`Searching for listing with ID ${listingId}`);
+
+  listedSongModel.find({_id: listingId}, (err, listing) => {
     if (err) {
       console.log(err);
       return res.status(400).json({ error: "Invalid listing ID" });
