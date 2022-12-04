d323d94c4c84a3a8419712dfb063d4f1568355a1
Rahul Hegde <rhegde.hrahul@gmail.com>
2022-12-04 15:53:04 -0500

    updated response messages

--- server/middleware/VerifyJwt.js
+++ server/middleware/VerifyJwt.js
@@ -20,16 +20,16 @@ const jwt = require("jsonwebtoken");
 const verifyJWT = (req, res, next) => {
   const authHeader = req.headers.authorization || req.headers.Authorization;
 
-  if (!authHeader?.startsWith("Bearer ")) return res.sendStatus(401);
+  if (!authHeader?.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "No authorization header" });
+  }
 
   const token = authHeader.split(" ")[1];
   console.log(`Verifying token: ${token}`);
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) {
-      //invalid token
-      console.log("Invalid token");
-      return res.sendStatus(403); 
+      return res.status(403).json({ message: "Invalid token" }); 
     }
 
     req.user = decoded.UserInfo.username;
--- server/middleware/VerifyRoles.js
+++ server/middleware/VerifyRoles.js
@@ -9,14 +9,18 @@
  */
 const verifyRoles = (...allowedRoles) => {
   return (req, res, next) => {
-    if (!req?.roles) return res.sendStatus(401);
+    if (!req?.roles){
+      return res.status(401).json({ message: "Unauthorized request (no roles found)" });
+    } 
 
     const rolesArray = [...allowedRoles];
     const result = req.roles
       .map((role) => rolesArray.includes(role))
       .find((val) => val === true);
 
-    if (!result) return res.sendStatus(403);
+    if (!result) {
+      return res.status(403).json({ message: "Forbidden request (insufficient clearance)" });
+    } 
     next();
   };
 };
